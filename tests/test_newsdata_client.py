af89e00e56a930df3f97c05263865e4265be89f2
Vikuuu <adivik672@gmail.com>
2025-09-15 14:34:53 +0530

    feat: Add tests for api's

---
+++ tests/test_newsdata_client.py
@@ -0,0 +1,56 @@
+import requests
+import unittest
+import os
+
+from newsdata_client import NewsDataClient
+from newsdata_client.newsdata_exception import NewsDataException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+class TestNewsDataClient(unittest.TestCase):
+    def test_latest(self):
+        client = get_newsdata_client()
+        try:
+            response = client.latest()
+        except Exception as e:
+            self.fail(e)
+
+    def test_incorrect_api_url(self):
+        client = get_newsdata_client()
+        client.latest_endpoint = client.latest_endpoint.replace("latest", "lat")
+
+        self.assertRaises(NewsDataException, client.latest)
+
+    def test_crpyto(self):
+        client = get_newsdata_client()
+        try:
+            response = client.crypto()
+        except Exception as e:
+            self.fail(e)
+
+    def test_archive(self):
+        client = get_newsdata_client()
+        try:
+            response = client.archive()
+        except Exception as e:
+            self.fail(e)
+
+    def test_sources(self):
+        client = get_newsdata_client()
+        try:
+            response = client.sources()
+        except Exception as e:
+            self.fail(e)
+
+
+def get_newsdata_client() -> NewsDataClient:
+    c = NewsDataClient(api_key=os.environ.get("API_KEY"))
+
+    return c
+
+
+if __name__ == "__main__":
+    unittest.main()
